2ce2c5a2befa96a302c8976946a8327230dc5978
andreyirk <sokolik_85@list.ru>
2017-12-04 16:58:07 +0700

    add new employee api

--- src/api/api.js
+++ src/api/api.js
@@ -10,6 +10,18 @@ export const departmentApi = {
 }
 
 
+export const employeeApi = {
+  register (userData) {
+    return fetch(`http://localhost:3000/employees`)
+        .then(statusHelper)
+        .then(response => response.json())
+        .catch(error => error)
+        .then(data => data)
+  }
+}
+
+
+
 function statusHelper (response) {
   if (response.status >= 200 && response.status < 300) {
     return Promise.resolve(response)
