5e0513bbf41b07f4c19e127f87685d1702a199bf
andreyirk <sokolik_85@list.ru>
2017-12-04 16:58:46 +0700

    change Error Feeld in common

--- src/components/common/ErrorField.js
+++ src/components/common/ErrorField.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function ErrorField(props) {
-  const {input, type, meta: {error, touched}} = props
+  const {input, type, label, meta: {error, touched}} = props
   const errorText = touched && error && <div style={{color: 'red'}}>{error}</div>
   return (
       <div className="">
-        <label>New Department Name</label>
+        <label>{label}</label>
         <input className="form-control" {...input} type={type}/>
         {errorText}
       </div>
