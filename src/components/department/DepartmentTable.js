a06eaa5bd381f795e4d60d70ebb191329fe6be07
andreyirk <sokolik_85@list.ru>
2017-12-04 16:41:51 +0700

    add react table to departments table component

--- src/components/department/DepartmentTable.js
+++ src/components/department/DepartmentTable.js
@@ -1,19 +1,69 @@
 import React, {Component} from 'react';
 import {departmentListSelector, fetchAllDepartments} from "../../redux/ducks/departmens";
 import {connect} from "react-redux";
+import ReactTable from 'react-table'
 
 
 class DepartmentTable extends Component {
-
+  constructor(props) {
+    super();
+    const { departments } = props
+    console.log('---in consturctuor--', departments)
+    this.state = {
+      data: departments
+    };
+    this.renderEditable = this.renderEditable.bind(this);
+  }
   componentDidMount(){
     this.props.fetchAllDepartments();
   }
 
+  renderEditable(cellInfo) {
+    console.log('---in render editagle--', cellInfo)
+    const { departments } = this.props;
+    return (
+        <div
+            style={{ backgroundColor: "#fafafa" }}
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={e => {
+              const data = [...departments];
+              data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+              this.props.chengeDepartmentName(data);
+            }}
+            dangerouslySetInnerHTML={{
+              __html: departments[cellInfo.index][cellInfo.column.id]
+            }}
+        />
+    );
+  }
+
   render() {
 
+    const { data } = this.state;
+    const { departments } = this.props;
+    console.log('departmensts in render', departments)
     return (
         <div className="row">
-          Departments table
+          <div className="col-md-10">
+            <ReactTable
+                data={departments}
+                columns={[
+
+                  {
+                    Header: "Name",
+                    accessor: "name",
+                    Cell: this.renderEditable
+                  }
+
+                ]}
+                defaultPageSize={5}
+                className="-striped -highlight"
+            />
+          </div>
+          <div className="col-md-2">
+
+          </div>
         </div>
     );
   }
